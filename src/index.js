c3ccae2c3c3f506ea0936abaee20634267a1f599
horex-pro <hosseinhorex@gmail.com>
2023-02-27 15:21:19 +0330

    start new version of that

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// import UserData from "./components/Product/UserInfo";
-import App from './App' 
-
-import "./index.css"
-
-ReactDOM.render(<App/>, document.getElementById("root"));
